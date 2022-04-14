1a1d926bc7af5d8734e5e7c2de238c29416c244d
kyang5 <kyang5@luc.edu>
2022-04-14 15:44:00 -0500

    Updated

--- COMP473-Project2-master/src/com/university/model/use/IUseSchedule.java
+++ COMP473-Project2-master/src/com/university/model/use/IUseSchedule.java
@@ -1,20 +1,19 @@
 package com.university.model.use;
 
-import com.university.model.facility.IFacilityLocation;
 import com.university.model.facility.IFacilityRoom;
 
 import java.util.List;
 
 public interface IUseSchedule {
-    public boolean isAtCapacity();
-    public int requestAvailableCapacity(IFacilityRoom room, IType facilityUseType);
-    public List<IUser> getListUsers();
-    public void setListUsers(List<IUser> listUsers);
-    public void assignUserToFacilityRoom(IUser user);
-    public void vacateFacilityRoom(IUser user);
-    public List<IType> getListActualUsage();
-    public void setListActualUsage(List<IType> listActualUsage);
-    public void addActualUsage(IType facilityUseType);
-    public void removeActualUsage(IType facilityUseType);
-    public long timeInterval(IType facilityUseType);
+    boolean isAtCapacity();
+    int requestAvailableCapacity(IFacilityRoom room, IUser user);
+    List<IUser> getListUsers();
+    void setListUsers(List<IUser> listUsers);
+    void assignUserToFacilityRoom(IUser user);
+    void vacateFacilityRoom(IUser user);
+    List<IType> getListActualUsage();
+    void setListActualUsage(List<IType> listActualUsage);
+    void addActualUsage(IType facilityUseType);
+    void removeActualUsage(IType facilityUseType);
+    long timeInterval(IType facilityUseType);
 }
