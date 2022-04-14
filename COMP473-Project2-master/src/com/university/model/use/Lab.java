fc8fd75a94b6ccdd35df9d384af917b8ee6a1772
kyang5 <kyang5@luc.edu>
2022-04-14 15:44:12 -0500

    Updated

--- COMP473-Project2-master/src/com/university/model/use/Lab.java
+++ COMP473-Project2-master/src/com/university/model/use/Lab.java
@@ -1,14 +1,60 @@
 package com.university.model.use;
 
-public class Lab extends Type{
+import com.university.model.facility.IFacilityRoom;
 
-    private String facilityUseType;
+import java.util.Date;
+
+public class Lab implements IType{
+
+    private int typeId;
+    private IFacilityRoom facilityRoom;
+    private Date useStartDate;
+    private Date useEndDate;
+    public int occupancy;
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    // assign facility room to use
+    public IFacilityRoom getFacilityRoom() {
+        return facilityRoom;
+    }
+
+    public void setFacilityRoom(IFacilityRoom facilityRoom) {
+        this.facilityRoom = facilityRoom;
+    }
+
+    public Date getUseStartDate() {
+        return useStartDate;
+    }
+
+    public void setUseStartDate(Date useStartDate) {
+        this.useStartDate = useStartDate;
+    }
+
+    public Date getUseEndDate() {
+        return useEndDate;
+    }
+
+    public void setUseEndDate(Date useEndDate) {
+        this.useEndDate = useEndDate;
+    }
+
+    public int getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
 
     public String getFacilityUseType() {
-        return facilityUseType;
+        return "Lab";
     }
 
-    public void setFacilityUseType() {
-        this.facilityUseType = "Lab";
-    }
 }
